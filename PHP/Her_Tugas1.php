5354c8d0550812133ca76c8a5b9dfa629ca5c84e
unknown <alqumaerah@gmail.com>
2025-04-29 11:34:38 +0800

    uplod tugas 1 hera

---
+++ PHP/Her_Tugas1.php
@@ -0,0 +1,10 @@
+<?php
+echo "nama saya hera saya lahir di Makassar,18 juli 2008
+saya menyelesaikan pendidikan di SD Impres Laikang pada tahun 2021 dan 
+SMPN 16 Makasssar pada tahun 2024,dan saya melanjutkan di 
+SMKIT Ibnul Qayyimm Makassar dan sekarang saya masih berada di kelas 10";
+
+print" nama saya hera saya lahir di Makassar,18 juli 2008
+saya menyelesaikan pendidikan di SD Impres Laikang pada tahun 2021 dan 
+SMPN 16 Makasssar pada tahun 2024,dan saya melanjutkan di 
+SMKIT Ibnul Qayyimm Makassar dan sekarang saya masih berada di kelas 10";
